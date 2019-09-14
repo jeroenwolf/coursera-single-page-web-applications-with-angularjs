80d5225333b42b563dd88a761295c6879af3a8dd
Jeroen Wolf <jeroenwolf76@hotmail.com>
2019-09-14 12:21:03 +0200

    Implemented bonus task 3

--- module5-solution/src/public/sign-up/sign-up.controller.js
+++ module5-solution/src/public/sign-up/sign-up.controller.js
@@ -10,11 +10,9 @@
         var $ctrl = this;
 
         $ctrl.submit = function (dish) {
-
             var promise = MenuService.getMenuItem(dish);
             promise.then(function (response) {
                 SignUpDataService.storeSignUpInfo($ctrl.user, response);
-
                 $ctrl.invalidFavoriteDish = false;
                 $ctrl.completed = true;
             }).catch(function (error) {
@@ -22,6 +20,23 @@
                 console.log(error);
             });
         };
+
+        $ctrl.validateFavoriteDish = function (form) {
+            if (form.dish && form.dish.$modelValue && form.dish.$modelValue.length > 0) {
+                var promise = MenuService.getMenuItem(form.dish.$modelValue);
+                promise.then(function (response) {
+                    $ctrl.invalidFavoriteDish = false;
+                    form.dish.$setValidity("text", true);
+                }).catch(function (error) {
+                    $ctrl.invalidFavoriteDish = true;
+                    form.dish.$setValidity("text", false);
+                    console.log(error);
+                });
+            } else {
+                $ctrl.invalidFavoriteDish = false;
+                form.dish.$setValidity("text", false);
+            }
+        }
     }
 
 
